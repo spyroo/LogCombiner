ab1a4e5101c6317faf46e7b51ffecb2502cfad33
Spyroo <declan.l.johnson@gmail.com>
2015-03-01 18:52:23 -0600

    improved file appending and reduced the amount of temp directories created

--- LogCombiner/src/io/spyroo/github/LogCombiner.java
+++ LogCombiner/src/io/spyroo/github/LogCombiner.java
@@ -1,16 +1,9 @@
 package io.spyroo.github;
 
-import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.nio.channels.Channels;
-import java.nio.channels.ReadableByteChannel;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
@@ -20,59 +13,33 @@ import net.lingala.zip4j.exception.ZipException;
 
 public class LogCombiner {
 	
+	private Path tempDir;
+	
 	public LogCombiner(){
-
-	}
-	/*
-	public void test(){
-		String clean = cleanLogsUrl("http://logs.tf/697809?highlight=76561198045043478");
-		String clean2 = cleanLogsUrl("http://logs.tf/697767?highlight=76561198045043478");
-		String url = getLogDownload(clean);
-		String url2 = getLogDownload(clean2);
 		try {
-			
-			File log1 = getLogFile(url, clean);
-			File log2 = getLogFile(url2, clean2);
-			
-			File combined = getCombinedFiles(log1, log2, "temp/newLogFile.log");
-			
-			System.out.println("done");
-			
+			tempDir = Files.createTempDirectory("Combiner_");
 		} catch (IOException e) {
 			e.printStackTrace();
-		} catch (ZipException e) {
-			e.printStackTrace();
 		}
-	}*/
+	}
 	
 	public File getLogFile(String logFileUrl, String cleanLogsLink) throws ZipException, IOException{
 		ZipFile zf = getZipFile(logFileUrl);
 		
-        Path tmp_2 = Files.createTempDirectory("Combiner_");
-		zf.extractAll(tmp_2.toAbsolutePath().toString());
+		zf.extractAll(tempDir.toAbsolutePath().toString());
 		
-		System.out.println(tmp_2.toString());
+		System.out.println(tempDir.toString());
 		System.out.println(getName(cleanLogsLink));
-		return new File(tmp_2.toString() + "/" + getName(cleanLogsLink));
+		return new File(tempDir.toString() + "/" + getName(cleanLogsLink));
 	}
 	
 	public File getCombinedFiles(File file1, File file2, String newLogFileName) throws IOException{
-		PrintWriter pw = new PrintWriter(newLogFileName);
-		BufferedReader br = new BufferedReader(new FileReader(file1));
-		String line;
-		System.out.println("adding file 1");
-		while((line = br.readLine()) != null){
-			pw.println(line);
-		}
-		br.close();
-		BufferedReader br2 = new BufferedReader(new FileReader(file2));
-		System.out.println("adding file 2");
-		while((line = br2.readLine()) != null){
-			pw.println(line);
-		}
-		br2.close();
-		pw.close();
-		return new File(newLogFileName);
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		out.write(Files.readAllBytes(file1.toPath()));
+		out.write(Files.readAllBytes(file2.toPath()));
+		File f = new File(newLogFileName);
+		Files.write(f.toPath(), out.toByteArray());
+		return f;
 	}
 	
 	public ZipFile getZipFile(String fileUrl) throws IOException, ZipException{
